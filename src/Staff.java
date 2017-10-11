d1f6bf7eb81866c6d4d402a8a88f4c15845f30dd
jemika <jemik.mail@gmail.com>
2017-10-11 15:21:03 +0300

    class Staff

---
+++ src/Staff.java
@@ -0,0 +1,26 @@
+public class Staff {
+    private String name;
+    private String post;
+
+    public Staff(String name, String post) {
+        this.name = name;
+        this.post = post;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getPost() {
+        return post;
+    }
+
+    public void setPost(String post) {
+        this.post = post;
+    }
+
+    @Override
+    public String toString() {
+        return (getName() + " on position " + getPost());
+    }
+}
