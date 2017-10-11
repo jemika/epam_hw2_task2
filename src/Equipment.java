64513246fea74193c39bc1176cc75a226bbbc5b3
jemika <jemik.mail@gmail.com>
2017-10-11 15:24:50 +0300

    clall Equipment

---
+++ src/Equipment.java
@@ -0,0 +1,32 @@
+public class Equipment {
+
+    private String name;
+    private int cost;
+
+    public Equipment(String name, int cost) {
+        this.name = name;
+        this.cost = cost;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getCost() {
+
+        return cost;
+    }
+
+    public void setCost(int cost) {
+        this.cost = cost;
+    }
+
+    @Override
+    public String toString() {
+        return "Equipment{" +
+                "name='" + name + '\'' +
+                ", cost=" + cost +
+                '}';
+    }
+
+}
